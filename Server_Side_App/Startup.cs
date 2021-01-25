37be8dbfa8fddd0d30cbec0af7fcae682b4586ed
Benjamin Zipperer <benjamin-zipperer@web.de>
2021-01-25 22:12:38 +0100

    Chang port Property back to 5001

--- Server_Side_App/Startup.cs
+++ Server_Side_App/Startup.cs
@@ -34,7 +34,7 @@ namespace Server_Side_App
                     .AllowAnyHeader()
                     .AllowCredentials());
             });
-            services.AddMvc();//.SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
